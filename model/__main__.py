cddade5dae3aa42d853d0c84ccceae9b252c77ae
itkum101 <077bct048.mukti@pcampus.edu.np>
2024-02-25 22:37:06 +0545

    minor change

--- model/__main__.py
+++ model/__main__.py
@@ -9,7 +9,6 @@ from network.client import send_model_for_optimization
 
 
 dotenv.load_dotenv(".env")
-_type = os.getenv("TYPE")
 _dataset = os.getenv("DATASET_PATH")
 
 
@@ -20,6 +19,11 @@ sleep_time = 20
 mlp = base.MLP()
 # global_model = copy.deepcopy(mlp)
 modelList = Queue()
+training_ips = ["20.121.57.95:80", "20.121.62.226:80", "172.208.98.225:80", "172.203.99.79:80"]
+
+def enqueue_model_list(model):
+    modelList.enqueue(model)
+    return True
 
 def training(global_model):
     dataset = torch.load(str(_dataset))
@@ -44,29 +48,38 @@ def traininig_test(_dataset_, global_model):
 def aggregating():
     #    time.sleep(10)
     global_model2 = copy.deepcopy(mlp)
-    accuracy = None
-    while not modelList.is_empty():
-        local_model = modelList.dequeue()
-        try:
+    accuracy = []
+    round=0
+    print("Run Main Here")
+    while True:
+        while not modelList.is_empty():
+            local_model = modelList.dequeue()
+            try:
 
-            pth = os.path.exists('global_model.pth')
+                pth = os.path.exists('global_model.pth')
 
-            if pth:
-                global_model2.load_state_dict(torch.load('global_model.pth'))
-            # try:
-            # except:
-            global_model3 = fed_avg_experiment(global_model=global_model2, local_model=local_model)
-            #     raise Exception("No global model found")
-            accuracy = validate(global_model3)
+                if pth:
+                    global_model2.load_state_dict(torch.load('global_model.pth'))
+                # try:
+                # except:
+                global_model3 = fed_avg_experiment(global_model=global_model2, local_model=local_model)
+                #     raise Exception("No global model found")
+                acc = validate(global_model3)
+                accuracy.append(acc)
+                torch.save(global_model3.state_dict(), 'global_model.pth')
+                for ip in training_ips:
+                    send_model_for_optimization(ip, global_model3)
+                print(" Global model updated successfully")
 
-            torch.save(global_model3.state_dict(), 'global_model.pth')
+            except:
+                modelList.enqueue(local_model)
+                raise Exception("Local model update failed. ")
+            
+            round +=1
 
-            print(" Global model updated successfully")
+            np.save(f"accuracy_{round}.npy", np.array(accuracy))
 
-        except:
-            modelList.enqueue(local_model)
-            raise Exception("Local model update failed. ")
-    return global_model3, accuracy
+
             
 
 
@@ -82,20 +95,19 @@ def train_test(global_model):
         print("Epoch : {} \n dataset {} \t Model Accuracy: {}".format(i,ith, acc))
 
 
-    np.save('accuracy_.npy+', np.array(accuracy))
+    np.save('accuracy.npy+', np.array(accuracy))
 
 
 
 
 def main():
-    if str(_type) == "training":
+    if str(os.getenv('TYPE')) == "training":
         global_model = copy.deepcopy(mlp)
         for i in range(10):
             training(global_model)
 
-    if str(_type) == "aggregation":
+    if str(os.getenv('TYPE')) == "aggregation":
         global_model = copy.deepcopy(mlp)
-        train_test(global_model=global_model)
-        
-
-main()
+        aggregating()
+                    
+# main()
